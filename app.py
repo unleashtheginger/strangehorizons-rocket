8193c02a3a173469d399e9a1e076f92702d920fa
Will Ellwood <fragmad@gmail.com>
2017-09-04 18:58:17 +0100

    Added a simple form and stuff

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from PIL import Image, ImageDraw
 import rocket
 
 app = Flask(__name__)
 
-@app.route('/rocket/')
+@app.route('/rocket/', methods=["POST"])
 @app.route('/rocket/<int:percentage>')
 def draw_rocket(percentage=50):
+    target = request.form['target']
+    current = request.form['current']
+
+    if request.method == 'POST':
+        percentage = (float(target) / 100) * float(current)
+
     img_io = rocket.draw_rocket(percentage, flask=True)
     img_io.seek(0)
     return send_file(img_io, mimetype='image/png')
 
+
+@app.route('/rocket/', methods=["GET"])
 @app.route('/')
 def index():
-    return 'Yo, it\'s working!'
-
+    return '''<!doctype html>
+    <title>Configure the Rocket!</title>
+    <h1>Configure!</h1>
+    <form method=post action='/rocket/'>
+      <p>
+    Target: <input type=text name=target>
+</p>
+<p>
+    Current: <input type=text name=current>
+</p>
+<p>
+         <input type=submit value=Upload>
+</p>
+    </form>
+    '''
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
