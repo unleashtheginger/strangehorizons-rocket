575720305fecf80ef1189ca1003d75158ce934e5
Will Ellwood <fragmad@gmail.com>
2017-09-07 22:05:06 +0100

    Transparent rocket and fill level added. Still slightly too rough.

--- rocket.py
+++ rocket.py
@@ -1,37 +1,47 @@
 from __future__ import print_function
+from copy import copy
 from PIL import Image, ImageDraw
 import StringIO
 
-BAR_BOTTOMX = 95
-BAR_BOTTOMY = 187
-BAR_TOPX_MAX = 105
-BAR_TOPY_MAX = 22
+# The bulb's widest point to the left is 50p5090# The bulb's widest point to the right 90px
+
+#BAR_BOTTOMX = 95
+BAR_BOTTOMX = 46
+#BAR_BOTTOMY = 187
+BAR_BOTTOMY = 211
+BAR_TOPX_MAX = 92
+BAR_TOPY_MAX = 25
 
 def draw_rocket(percentage=0, flask=False):
-    img = Image.open('sad-rocket.png')
-#    img = Image.open('rocket.jpg')
+#    img = Image.open('sad-rocket.png')
+    foreground = Image.open('rocket.png')
+    foreground = foreground.convert('RGBA')
+    x, y, = foreground.size
+    background = Image.new('RGBA', (x, y), (255, 255, 255, 255))
+
 #    image.show()
 
     percentage_diff = 100 - percentage
-    print('percentage_diff', percentage_diff)
+#    print('percentage_diff', percentage_diff)
 
     pixel_range = BAR_BOTTOMY - BAR_TOPY_MAX
     percentage_height = ((pixel_range / 100.0) * percentage)
-    print('percentage height:', percentage_height)
+#    print('percentage height:', percentage_height)
 
     bar_topy = BAR_BOTTOMY - percentage_height
-    print('bar_topy', bar_topy)
+#    print('bar_topy', bar_topy)
 
-    blank_image = img
-    img_draw = ImageDraw.Draw(blank_image)
-    img_draw.rectangle((95, 187, 105, bar_topy), outline='blue', fill='blue')
+    img_draw = ImageDraw.Draw(background)
+    img_draw.rectangle((BAR_BOTTOMX, BAR_BOTTOMY, BAR_TOPX_MAX, bar_topy),
+                       outline='blue', fill='blue')
+    background.paste(foreground, (0, 0), foreground)
 
     if flask:
         img_io = StringIO.StringIO()
-        blank_image.save(img_io, 'PNG', quality=100)
+        background.save(img_io, 'PNG', quality=100)
         return img_io
     else:
-        return blank_image
+        return background
 
 if __name__ == '__main__':
     image = draw_rocket()
